c015078a100b439c4656316a9c505d1562441ef2
Pedro Henrique Lopes <phs@lionx.com.br>
2022-04-18 11:01:54 -0300

    p2 exam update

--- src/exercicio1.js
+++ src/exercicio1.js
@@ -2,19 +2,19 @@ let letra = prompt("Digite uma letra: ");
 
 switch (letra) {
   case "a": case "A":
-    alert("Aletra digitada é uma vogal.")
+    alert("A letra digitada é uma vogal.")
     break
   case "e": case "E":
-    alert("Aletra digitada é uma vogal.")
+    alert("A letra digitada é uma vogal.")
     break
   case "i": case "I":
-    alert("Aletra digitada é uma vogal.")
+    alert("A letra digitada é uma vogal.")
     break
   case "o": case "O":
-    alert("Aletra digitada é uma vogal.")
+    alert("A letra digitada é uma vogal.")
     break
   case "u": case "U":
-    alert("Aletra digitada é uma vogal.")
+    alert("A letra digitada é uma vogal.")
     break
   default:
     alert("A letra digitada é uma consoante.")
